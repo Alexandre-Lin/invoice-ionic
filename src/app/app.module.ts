ed16e5ab74f881790910e7728cbcd894fad56ef4
Alexandre Lin <alexandre.lin@okayo.fr>
2022-01-13 17:43:29 +0100

    Adding print button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
-import { Drivers } from '@ionic/storage';
-import { IonicStorageModule } from '@ionic/storage-angular';
+import {Drivers} from '@ionic/storage';
+import {IonicStorageModule} from '@ionic/storage-angular';
 import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
-import { PreviewPrintComponent } from './preview-print/preview-print.component';
+import {PreviewPrintComponent} from './preview-print/preview-print.component';
 
 import {File} from '@ionic-native/file/ngx';
 import {FileTransfer} from '@ionic-native/file-transfer/ngx';
 import {DocumentPicker} from '@ionic-native/document-picker/ngx';
+import {Printer} from '@ionic-native/printer/ngx';
 
 
 export function createTranslateLoader(http: HttpClient) {
@@ -26,26 +27,27 @@ export function createTranslateLoader(http: HttpClient) {
 }
 
 @NgModule({
-  declarations: [AppComponent,PreviewPrintComponent],
+  declarations: [AppComponent, PreviewPrintComponent],
   entryComponents: [],
   imports: [BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
-    IonicStorageModule.forRoot( {
+    IonicStorageModule.forRoot({
       name: "invoice_ionic_db",
       driverOrder: [CordovaSQLiteDriver._driver, Drivers.IndexedDB, Drivers.LocalStorage]
     }),
     TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      },
-      defaultLanguage: "fr"
-    }
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        },
+        defaultLanguage: "fr"
+      }
     )],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, File, FileTransfer, DocumentPicker],
+  providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}, File, FileTransfer, DocumentPicker, Printer],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/preview-print/preview-print.component.ts
+++ src/app/preview-print/preview-print.component.ts
@@ -4,6 +4,7 @@ import {TranslateService} from '@ngx-translate/core';
 import * as moment from 'moment';
 import {StorageServiceService} from '../services/storage-service.service';
 import {Invoice} from '../shared/model/invoice';
+import {Printer, PrintOptions} from '@ionic-native/printer/ngx';
 
 @Component({
   selector: 'app-preview-print',
@@ -31,7 +32,8 @@ export class PreviewPrintComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private storageService: StorageServiceService,
-              private translateService: TranslateService) {
+              private translateService: TranslateService,
+              private printer: Printer) {
   }
 
   ngOnInit() {
@@ -50,5 +52,27 @@ export class PreviewPrintComponent implements OnInit {
   // change label of payment mode for printing
   convertPaymentModeLabel(): void {
     this.currentInvoice.paymentMode = this.translateService.instant(this.currentInvoice.paymentMode);
+  }
+
+  /**
+   * To print the generated invoice
+   */
+  print(): void {
+    this.printer.isAvailable().then((onSuccess: any) => {
+      const content = document.getElementById('invoiceDocument').innerHTML;
+      const options: PrintOptions = {
+        name: 'invoice',
+        duplex: true,
+        orientation: 'portrait',
+        monochrome: true
+      };
+      this.printer.print(content, options).then((value: any) => {
+        console.log('value:', value);
+      }, (error) => {
+        console.log('error:', error);
+      });
+    }, (err) => {
+      console.log('error:', err);
+    });
   }
 }
